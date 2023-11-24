8ef4e09c74ffba8b194c931c05a5c355237558b2
Diksha <dikshat0602@gmail.com>
2023-11-24 16:19:30 +0530

    added

--- index.js
+++ index.js
@@ -3,41 +3,38 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = 8000;
-require('dotenv').config();
-require("./db")
-const cookieParser = require('cookie-parser');
-const authRoutes = require('./ROUTES/Auth');
-const adminRoutes = require('./ROUTES/Admin');
-const movieRoutes = require('./ROUTES/Movie');
-const imageuploadRoutes = require('./ROUTES/imageUploadRoutes');
+require("dotenv").config();
+require("./db");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./ROUTES/Auth");
+const adminRoutes = require("./ROUTES/Admin");
+const movieRoutes = require("./ROUTES/Movie");
+const imageuploadRoutes = require("./ROUTES/imageUploadRoutes");
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-const allowedOrigins = ['http://localhost:3000','http://localhost:3001']; // Add more origins as needed
+const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"]; // Add more origins as needed
 app.use(
   cors({
-      origin: function (origin, callback) {
-          if (!origin || allowedOrigins.includes(origin)) {
-              callback(null, true);
-          } else {
-              callback(new Error('Not allowed by CORS'));
-          }
-      },
-      credentials: true, // Allow credentials
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Allow credentials
   })
 );
 
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 app.use("/admin", adminRoutes);
-app.use('/movie', movieRoutes);
-app.use('/image', imageuploadRoutes);
-
+app.use("/movie", movieRoutes);
+app.use("/image", imageuploadRoutes);
 
 app.get("/", (req, res) => {
   res.json("The Backend is working");
 });
-
-
 
 app.listen(PORT, (req, res) => {
   console.log(`Server is running on port ${PORT}`);

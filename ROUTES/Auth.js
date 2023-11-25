f1f0e846050acdeeea383567a513f5a8d7062348
Diksha <dikshat0602@gmail.com>
2023-11-25 13:14:44 +0530

    added

--- ROUTES/Auth.js
+++ ROUTES/Auth.js
@@ -1,125 +1,136 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../Models/UserSchema');
-const errorHandler = require('../Middlewares/errorMiddleware');
-const authTokenHandler = require('../Middlewares/checkAuthToken');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const nodemailer = require('nodemailer');
+const User = require("../MODELS/UserSchema");
+const errorHandler = require("../Middlewares/errorMiddleware");
+const authTokenHandler = require("../Middlewares/checkAuthToken");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
 
-
-
-router.get('/test', async (req, res) => {
-    res.json({
-        message: "Auth api is working"
-    })
-})
-
-
+router.get("/test", async (req, res) => {
+  res.json({
+    message: "Auth api is working",
+  });
+});
 
 function createResponse(ok, message, data) {
-    return {
-        ok,
-        message,
-        data,
-    };
+  return {
+    ok,
+    message,
+    data,
+  };
 }
 
-router.post('/register', async (req, res, next) => {
-    try {
-        const { name, email, password, city } = req.body;
-        const existingUser = await User.findOne({ email: email });
-        if (existingUser) {
-            return res.status(409).json(createResponse(false, 'Email already exists'));
-        }
-
-        const newUser = new User({
-            name,
-            password,
-            email,
-            city
-        });
-
-        await newUser.save(); // Await the save operation
-        res.status(201).json(createResponse(true, 'User registered successfully'));
-
+router.post("/register", async (req, res, next) => {
+  try {
+    const { name, email, password, city } = req.body;
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      return res
+        .status(409)
+        .json(createResponse(false, "Email already exists"));
     }
-    catch (err) {
-        next(err)
-    }
-})
+
+    const newUser = new User({
+      name,
+      password,
+      email,
+      city,
+    });
+
+    await newUser.save(); // Await the save operation
+    res.status(201).json(createResponse(true, "User registered successfully"));
+  } catch (err) {
+    next(err);
+  }
+});
 
 // change user city
-router.post('/changeCity', authTokenHandler, async (req, res, next) => {
-    const { city } = req.body;
-    const user = await User.findOne({ _id: req.userId });
+router.post("/changeCity", authTokenHandler, async (req, res, next) => {
+  const { city } = req.body;
+  const user = await User.findOne({ _id: req.userId });
 
-    if (!user) {
-        return res.status(400).json(createResponse(false, 'Invalid credentials'));
-    }
-    else{
-        user.city = city;
-        await user.save();
-        return res.status(200).json(createResponse(true, 'City changed successfully'));
-    }
-})
+  if (!user) {
+    return res.status(400).json(createResponse(false, "Invalid credentials"));
+  } else {
+    user.city = city;
+    await user.save();
+    return res
+      .status(200)
+      .json(createResponse(true, "City changed successfully"));
+  }
+});
 
 // router.post('/sendotp', async (req, res) => {})
-router.post('/login', async (req, res, next) => {
-    console.log(req.body);
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) {
-        console.log('user not found');
-        return res.status(400).json(createResponse(false, 'Invalid credentials'));
-    }
+router.post("/login", async (req, res, next) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    console.log("user not found");
+    return res.status(400).json(createResponse(false, "Invalid credentials"));
+  }
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-        console.log('password not matched');
-        return res.status(400).json(createResponse(false, 'Invalid credentials'));
-    }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    console.log("password not matched");
+    return res.status(400).json(createResponse(false, "Invalid credentials"));
+  }
 
-    const authToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '30m' });
-    const refreshToken = jwt.sign({ userId: user._id }, process.env.JWT_REFRESH_SECRET_KEY, { expiresIn: '40m' });
-    res.cookie('authToken', authToken,  { httpOnly: true, secure: true, sameSite: 'None' });
-    res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, sameSite: 'None' });
+  const authToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "30m",
+  });
+  const refreshToken = jwt.sign(
+    { userId: user._id },
+    process.env.JWT_REFRESH_SECRET_KEY,
+    { expiresIn: "40m" }
+  );
+  res.cookie("authToken", authToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+  });
 
-    res.status(200).json(createResponse(true, 'Login successful', {
-        authToken,
-        refreshToken
-    }));
-})
-
-router.get('/checklogin', authTokenHandler, async (req, res) => {
-    res.json({
-        userId: req.userId,
-        ok: true,
-        message: 'User authenticated successfully'
+  res.status(200).json(
+    createResponse(true, "Login successful", {
+      authToken,
+      refreshToken,
     })
-})
+  );
+});
 
+router.get("/checklogin", authTokenHandler, async (req, res) => {
+  res.json({
+    userId: req.userId,
+    ok: true,
+    message: "User authenticated successfully",
+  });
+});
 
-router.get('/logout', authTokenHandler, async (req, res) => {
-    res.clearCookie('authToken');
-    res.clearCookie('refreshToken');
-    res.json({
-        ok: true,
-        message: 'User logged out successfully'
-    })
-})
+router.get("/logout", authTokenHandler, async (req, res) => {
+  res.clearCookie("authToken");
+  res.clearCookie("refreshToken");
+  res.json({
+    ok: true,
+    message: "User logged out successfully",
+  });
+});
 
-router.get('/getuser', authTokenHandler, async (req, res) => {
-    const user = await User.findOne({ _id: req.userId });
+router.get("/getuser", authTokenHandler, async (req, res) => {
+  const user = await User.findOne({ _id: req.userId });
 
-    if (!user) {
-        return res.status(400).json(createResponse(false, 'Invalid credentials'));
-    }
-    else{
-        return res.status(200).json(createResponse(true, 'User found', user));
-    }
-})
+  if (!user) {
+    return res.status(400).json(createResponse(false, "Invalid credentials"));
+  } else {
+    return res.status(200).json(createResponse(true, "User found", user));
+  }
+});
 
-router.use(errorHandler)
+router.use(errorHandler);
 
 module.exports = router;
